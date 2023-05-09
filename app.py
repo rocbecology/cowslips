4820c8a229c7d49ab32feb906112a5ea4b9ad3bb
rocbecology <132952698+rocbecology@users.noreply.github.com>
2023-05-09 11:05:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,12 +40,13 @@ def image(image_index):
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    user_id = "example_user_id"  # Replace this with a user ID from your application, e.g., session ID
     image_id = request.form.get('image_id')
     image_url = request.form.get('image_url')
     flower_type = request.form.get('flower_type')
 
     # Insert the submitted data into the database
-    cur.execute("INSERT INTO responses (image_id, image_url, response) VALUES (%s, %s, %s)", (image_id, image_url, flower_type))
+    cur.execute("INSERT INTO responses (user_id, image_id, image_url, response) VALUES (%s, %s, %s, %s)", (user_id, image_id, image_url, flower_type))
     conn.commit()
 
     image_index = images[images['ID'] == int(image_id)].index[0]
