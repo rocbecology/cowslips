f44a200e2690b35a07d4e9f33e309d7bb08fb805
rocbecology <132952698+rocbecology@users.noreply.github.com>
2023-05-09 07:11:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import pandas as pd
 import csv
 from flask import Flask, render_template, request, redirect, url_for
 from random import sample
+import psycopg2
+
+# Connect to the PostgreSQL database
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
 
 # Load the CSV file with image IDs and URLs
 images = pd.read_csv('images.csv')
@@ -11,9 +17,6 @@ images = pd.read_csv('images.csv')
 images = images.sample(frac=1).reset_index(drop=True)
 
 app = Flask(__name__)
-
-# Create a variable for the absolute path to the responses.csv file
-responses_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'responses.csv')
 
 @app.route('/image/<int:image_index>')
 def image(image_index):
@@ -27,24 +30,15 @@ def submit():
     image_id = request.form.get('image_id')
     image_url = request.form.get('image_url')
     flower_type = request.form.get('flower_type')
-    
-    # Read the responses.csv file
-    responses = pd.read_csv(responses_file_path)
 
-    # Create a new DataFrame with the submitted data
-    new_response = pd.DataFrame({'ID': [image_id], 'URL': [image_url], 'Response': [flower_type]})
-    
-    # Append the new response to the responses DataFrame
-    responses = pd.concat([responses, new_response], ignore_index=True)
-
-    # Save the updated responses DataFrame to the CSV file
-    responses.to_csv(responses_file_path, index=False)
+    # Insert the submitted data into the database
+    cur.execute("INSERT INTO responses (id, url, response) VALUES (%s, %s, %s)", (image_id, image_url, flower_type))
+    conn.commit()
 
     image_index = images[images['ID'] == int(image_id)].index[0]
     next_image_index = (image_index + 1) % len(images)
 
     return redirect(url_for('image', image_index=next_image_index, message="Thank you for your submission!"))
-
 
 @app.route('/')
 def index():
