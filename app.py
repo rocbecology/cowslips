b95a186e0fed61f80921954979fb1c0bd5c58403
rocbecology <132952698+rocbecology@users.noreply.github.com>
2023-05-09 11:14:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def index():
     return redirect(url_for('image', image_index=0))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=os.environ.get("FLASK_DEBUG", False), host='0.0.0.0', port=5000)
